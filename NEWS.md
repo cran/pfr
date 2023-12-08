9ad6ce48dbfac9b2ff3398ee98860815341af209
Taylor Brown <trb5me@virginia.edu>
2023-12-08 17:40:02 +0000

    version 1.0.1

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# pfr 1.0.1
+
+* Addressed bug regarding vignette generation.
+
 # pfr 1.0.0
 
 * Initial submission to CRAN
--- inst/doc/my-vignette.R
+++ inst/doc/my-vignette.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -9,10 +9,10 @@ library(pfr)
 # requires pandoc-citeproc, btu no way to make user download that
 #knitr::opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
 
-## ---- out.width="100%", echo=F------------------------------------------------
+## ----out.width="100%", echo=F-------------------------------------------------
 knitr::include_graphics("ssm_diagram_1-1.png")
 
-## ---- eval = FALSE------------------------------------------------------------
+## ----eval = FALSE-------------------------------------------------------------
 #  library(pfr)
 #  createPFCPPTemplates("svol_leverage", "BSWC", fileDir = "./")
 
@@ -23,18 +23,18 @@ knitr::include_graphics("ssm_diagram_1-1.png")
 #  svol_lev$svol_leverage_bswc_approx_LL(rnorm(100), c(.9, 0.0, 1.0, -.2))
 #  svol_lev$svol_leverage_bswc_approx_filt(rnorm(100), c(.9, 0.0, 1.0, -.2))
 
-## ---- eval = FALSE------------------------------------------------------------
+## ----eval = FALSE-------------------------------------------------------------
 #  createPFCPPTemplates("mean_factor_msvol", "APF", fileDir = "./")
 
 ## -----------------------------------------------------------------------------
 fsvol <- buildModelFuncs(".", "mean_factor_msvol")
 
-## -----------------------------------------------------------------------------
-paramEst <- c(1.22, #B
-              -1.17, -.01, .08, #mu
-              .85, .84, .81, #Phi
-              3.7, 3.4, 3.49) #Sigma
-fakeData <- matrix(rnorm(50), ncol=2)
-fsvol$mean_factor_msvol_apf_approx_LL(fakeData, paramEst)
-fsvol$mean_factor_msvol_apf_approx_filt(fakeData, paramEst)
+## ----eval = FALSE-------------------------------------------------------------
+#  paramEst <- c(1.22, #B
+#                -1.17, -.01, .08, #mu
+#                .85, .84, .81, #Phi
+#                3.7, 3.4, 3.49) #Sigma
+#  fakeData <- matrix(rnorm(50), ncol=2)
+#  fsvol$mean_factor_msvol_apf_approx_LL(fakeData, paramEst)
+#  fsvol$mean_factor_msvol_apf_approx_filt(fakeData, paramEst)
 
